048e6e3522a1fdc2724e487f1497f117644b1a48
Radostin Bonchev <radostinbonchev@gmail.com>
2023-01-31 13:38:14 +0200

    Adding game's code

---
+++ code/guess_a_number.py
@@ -0,0 +1,32 @@
+import random
+
+computer_number = random.randint(1, 100)
+
+attempts = 10
+
+for attempt in range(4):
+    player_input = input("Guess the number between 1 and 100: ")
+    attempts -= 1
+    print(f"Attempts left: {attempts}")
+
+    if not player_input.isdigit():
+        print("Invalid Input. Please enter a number!")
+        continue
+
+    player_input = int(player_input)
+
+    if player_input == computer_number:
+        print(f"You guessed it!")
+        break
+
+    if player_input > computer_number:
+        print("Number is too high!")
+    elif player_input < computer_number:
+        print("Number is too low!")
+
+play_again_input = input("Type [yes] if you want to play again or [no] to quit: ")
+
+if play_again_input == "yes":
+    exec(open("guess_a_number.py").read())
+else:
+    print("Thank you for playing!")
